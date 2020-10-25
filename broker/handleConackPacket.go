6d0d567ae376b94757ff5d75d5bd716ae73c303e
stackshadow <stackshadow@evilbrain.de>
2020-10-25 10:12:09 +0000

    Register client in handleConackPacket to support client-side-code

--- broker/handleConackPacket.go
+++ broker/handleConackPacket.go
@@ -1,6 +1,15 @@
 package broker
 
+import "gitlab.com/mgtt/plugin"
+
 func (broker *Broker) handleConackPacket(event *Event) (err error) {
 	event.client.Connected = true
+	broker.clients[event.client.ID()] = event.client
+
+	// PLUGINS: call CallOnAcceptNewClient - check if we accept the client
+	if err == nil {
+		plugin.CallOnConnack(event.client.ID())
+	}
+
 	return
 }
