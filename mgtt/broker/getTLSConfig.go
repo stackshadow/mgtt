59e3df11ab60a9e5fe5ce02b969e745681a4b369
stackshadow <stackshadow@evilbrain.de>
2020-11-05 20:47:16 +0000

    [TLS] Use only PFS-Ciphers

--- mgtt/broker/getTLSConfig.go
+++ mgtt/broker/getTLSConfig.go
@@ -44,6 +44,14 @@ func getTLSConfig(config Config) (cfg *tls.Config, err error) {
 		Certificates:       []tls.Certificate{cert},
 		InsecureSkipVerify: true,
 		ClientCAs:          clientCAs,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		},
 	}
 
 	if config.CAFile != "" {
