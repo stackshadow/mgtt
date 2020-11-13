790122c5d0adbfc5551a13d83f0ce6bf3e6947a1
stackshadow <stackshadow@evilbrain.de>
2020-11-13 19:26:14 +0000

    [FIX] Multiple publish to subscriber when multiple topic-filter matches

--- mgtt/client/Publish.go
+++ mgtt/client/Publish.go
@@ -30,7 +30,10 @@ func (c *MgttClient) Publish(pubpacket *packets.PublishPacket) (published bool, 
 
 	topic := pubpacket.TopicName
 	topicArray := strings.Split(topic, "/")
+	topicMatched := false
 
+	// check if one of our subscription matched
+	// this prevents multiple sending of packet to a single client
 	for _, subscriptionTopic := range c.subscriptionTopics {
 		subscriptionTopicArray := strings.Split(subscriptionTopic, "/")
 
@@ -38,18 +41,21 @@ func (c *MgttClient) Publish(pubpacket *packets.PublishPacket) (published bool, 
 		// The Topic Name in a PUBLISH Packet sent by a Server to a subscribing Client
 		// MUST match the Subscription’s Topic Filter
 		if Match(subscriptionTopicArray, topicArray) == true {
-
-			log.Info().
-				Str("clientid", c.ID()).
-				Uint16("mid", pubpacket.MessageID).
-				Str("topic", pubpacket.TopicName).
-				Msg("Publish message to client")
-
-			err = pubpacket.Write(c.connection)
-			published = true
-			return
+			topicMatched = true
+			break
 		}
 
+	}
+
+	if topicMatched == true {
+		log.Info().
+			Str("clientid", c.ID()).
+			Uint16("mid", pubpacket.MessageID).
+			Str("topic", pubpacket.TopicName).
+			Msg("Publish message to client")
+
+		err = pubpacket.Write(c.connection)
+		published = true
 	}
 
 	return
