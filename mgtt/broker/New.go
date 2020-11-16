ebf2ec266073fa50d1ce7d41714dc86d1dc7ad51
stackshadow <stackshadow@evilbrain.de>
2020-11-16 15:38:37 +0000

    ADD PublishToClient to support publish to an specific client

--- mgtt/broker/New.go
+++ mgtt/broker/New.go
@@ -4,12 +4,18 @@ import (
 	"gitlab.com/mgtt/client"
 )
 
+// Current is the last created broker
+var Current *Broker = nil
+
 // New will create a new Broker
 func New() (broker *Broker, err error) {
 	broker = &Broker{
 		clients:      make(map[string]*client.MgttClient),
 		clientEvents: make(chan *Event, 10),
 	}
+
+	// remember the current broker
+	Current = broker
 
 	return
 }
