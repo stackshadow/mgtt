e48f78c6ad48b21ca74e38142d2429fcee4a4c48
stackshadow <stackshadow@evilbrain.de>
2021-01-04 20:42:35 +0000

    [auth] Send error on auth-failure
    - when an mqtt-client don't have an permission to publish to a topic, '$SYS/self/error' will send to the client

--- internal/mgtt-plugins/acl/OnPublishRequest.go
+++ internal/mgtt-plugins/acl/OnPublishRequest.go
@@ -1,6 +1,23 @@
 package acl
 
+import (
+	"fmt"
+
+	"gitlab.com/mgtt/internal/mgtt/broker"
+)
+
 // OnPublishRequest get called when an publisher try to publish to the broker
 func OnPublishRequest(clientID string, username string, topic string) (accepted bool) {
-	return checkACL(clientID, username, topic, "w")
+
+	allowed := checkACL(clientID, username, topic, "w")
+
+	if allowed == false {
+		broker.Current.PublishToClient(
+			clientID,
+			"$SYS/self/error",
+			[]byte(fmt.Sprintf("Access to '%s' denied", topic)),
+		)
+	}
+
+	return allowed
 }
