4733c9cbeb45d0ea64d2600df0cf9a41ea782c61
stackshadow <stackshadow@evilbrain.de>
2021-02-09 20:22:47 +0000

    [client] Move machter-function to separate file

--- internal/mgtt/client/Publish.go
+++ internal/mgtt/client/Publish.go
@@ -7,35 +7,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// MatchRoute compare an route with an topic
-func MatchRoute(route string, topic string) (match bool) {
-
-	routeArray := strings.Split(route, "/")
-	topicArray := strings.Split(topic, "/")
-
-	return Match(routeArray, topicArray)
-}
-
-// Match compare an selector route with an given topic
-func Match(route []string, topic []string) bool {
-	if len(route) == 0 {
-		return len(topic) == 0
-	}
-
-	if len(topic) == 0 {
-		return route[0] == "#"
-	}
-
-	if route[0] == "#" {
-		return true
-	}
-
-	if (route[0] == "+") || (route[0] == topic[0]) {
-		return Match(route[1:], topic[1:])
-	}
-	return false
-}
-
 // Publish an publish-packet
 //
 // this function check if the topic in the `packet` matches the client-topic-filter
