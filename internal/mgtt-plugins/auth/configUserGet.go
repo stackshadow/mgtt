f32ffb7370fc64553d93437094e08898fa18e572
stackshadow <stackshadow@evilbrain.de>
2021-04-04 22:03:30 +0000

    [auth] Return userinfo for anonymouse-user

--- internal/mgtt-plugins/auth/configUserGet.go
+++ internal/mgtt-plugins/auth/configUserGet.go
@@ -2,6 +2,15 @@ package auth
 
 // configUserGet return the user without password
 func configUserGet(username string) (user pluginConfigUser, exist bool) {
+
+	if username == "" {
+		user.Username = "_anonym"
+		user.Password = ""
+
+		exist = true
+		return
+	}
+
 	if user, exist = config.Users[username]; exist {
 		user.Username = username
 		user.Password = ""
